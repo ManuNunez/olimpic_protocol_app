cac0864f1f546f13fdc46788c475477012ac73c1
MNunez <manu.nunez.2004@gmail.com>
2024-02-04 16:20:07 -0600

    feat : login form added as a component

---
+++ src/view/components/login_form.php
@@ -0,0 +1,19 @@
+<div class="bg-gray-800 p-8 rounded shadow-md w-96">
+    <div class="text-center mb-6">
+        <h2 class="text-2xl font-semibold">Iniciar Sesión</h2>
+    </div>
+
+    <form>
+        <div class="mb-4">
+            <label for="username" class="block text-sm font-medium text-gray-300">Nombre de Usuario</label>
+            <input type="text" id="username" name="username" class="mt-1 p-2 w-full bg-gray-700 text-white border rounded-md">
+        </div>
+
+        <div class="mb-4">
+            <label for="password" class="block text-sm font-medium text-gray-300">Contraseña</label>
+            <input type="password" id="password" name="password" class="mt-1 p-2 w-full bg-gray-700 text-white border rounded-md">
+        </div>
+
+        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md w-full">Iniciar Sesión</button>
+    </form>
+</div>
