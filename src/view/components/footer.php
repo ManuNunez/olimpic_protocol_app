6f014d87f1212b6baff0b4cd9f252505dcab7424
MNunez <manu.nunez.2004@gmail.com>
2024-02-04 16:38:16 -0600

    tailwind rebuild

--- src/view/components/footer.php
+++ src/view/components/footer.php
@@ -1,3 +1,3 @@
 <footer class="bg-gray-800 text-white text-center py-4 fixed bottom-0 w-full">
-    <p>&copy; 2024 Tu Empresa. Todos los derechos reservados.</p>
+    <p>&copy; 2024 FLE_SYSTEMS. Todos los derechos reservados.</p>
 </footer>
