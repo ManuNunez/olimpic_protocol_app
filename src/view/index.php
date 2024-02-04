390922f3be39a2807fa41f5c784f84df2a751081
MNunez <manu.nunez.2004@gmail.com>
2024-02-04 16:38:49 -0600

    feat : tailwind inlcuded in index

--- src/view/index.php
+++ src/view/index.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['usuario'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Math Contest Protocol</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="resources/main.css" rel="stylesheet">
 </head>
 
 <body>
