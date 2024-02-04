b955dfbe3b49316b7a6bbe3a46bd134302190aa3
MNunez <manu.nunez.2004@gmail.com>
2024-02-04 16:09:08 -0600

    feat : index.php

---
+++ src/view/index.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+/*
+if (!isset($_SESSION['usuario'])) {
+    header("Location: login.php");
+    exit();
+}
+*/
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Math Contest Protocol</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body>
+<?php
+    include('components/nav.php');
+?>
+
+</body>
