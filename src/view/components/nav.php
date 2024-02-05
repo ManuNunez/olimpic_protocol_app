ba7b90c0a4b1dff204c19642e3e39610a9af8ec3
MNunez <manu.nunez.2004@gmail.com>
2024-02-04 19:47:42 -0600

    refactor : nav is now  using the contentSelector

--- src/view/components/nav.php
+++ src/view/components/nav.php
@@ -1,9 +1,7 @@
+<!-- nav.php -->
 <nav class="bg-blue-500 p-4">
     <div class="container mx-auto flex justify-between items-center">
-
-        <div class="flex items-center">
-            <a href="#" class="text-white font-bold text-lg">Inicio</a>
-        </div>
+        <a href="#" class="text-white font-bold text-lg">Inicio</a>
 
         <div class="lg:hidden">
             <button id="menu-toggle" class="text-white focus:outline-none">
@@ -14,26 +12,37 @@
         </div>
 
         <div class="hidden lg:flex space-x-4">
-            <a href="#" class="text-white">Chat</a>
-            <a href="#" class="text-white">Cronómetro</a>
-            <a href="#" class="text-white">Lista de Asistencia</a>
-            <a href="#" class="text-white">Registro</a>
-            <a href="#" class="text-white">Olimpiadas</a>
-            <a href="#" class="text-white">Sedes</a>
-            <a href="#" class="text-white">Cuentas</a>
+            <a href="?section=chat" class="text-white">Chat</a>
+            <a href="?section=cronometro" class="text-white">Cronómetro</a>
+            <a href="?section=listaAsistencia" class="text-white">Lista de Asistencia</a>
+            <a href="?section=registro" class="text-white">Registro</a>
+            <a href="?section=olimpiadas" class="text-white">Olimpiadas</a>
+            <a href="?section=sedes" class="text-white">Sedes</a>
+            <a href="?section=cuentas" class="text-white">Cuentas</a>
         </div>
+    </div>
+
+    <div id="mobile-menu" class="lg:hidden hidden bg-white p-4">
+        <a href="?section=chat" class="block text-gray-800 mb-2">Chat</a>
+        <a href="?section=cronometro" class="block text-gray-800 mb-2">Cronómetro</a>
+        <a href="?section=listaAsistencia" class="block text-gray-800 mb-2">Lista de Asistencia</a>
+        <a href="?section=registro" class="block text-gray-800 mb-2">Registro</a>
+        <a href="?section=olimpiadas" class="block text-gray-800 mb-2">Olimpiadas</a>
+        <a href="?section=sedes" class="block text-gray-800 mb-2">Sedes</a>
+        <a href="?section=cuentas" class="block text-gray-800 mb-2">Cuentas</a>
     </div>
 </nav>
 
-<div id="mobile-menu" class="lg:hidden hidden bg-white p-4">
-    <a href="#" class="block text-gray-800 mb-2">Chat</a>
-    <a href="#" class="block text-gray-800 mb-2">Cronómetro</a>
-    <a href="#" class="block text-gray-800 mb-2">Lista de Asistencia</a>
-    <a href="#" class="block text-gray-800 mb-2">Registro</a>
-    <a href="#" class="block text-gray-800 mb-2">Olimpiadas</a>
-    <a href="#" class="block text-gray-800 mb-2">Sedes</a>
-    <a href="#" class="block text-gray-800 mb-2">Cuentas</a>
-</div>
+<?php
+include_once 'selector.php';
 
+$section = isset($_GET['section']) ? $_GET['section'] : '';
 
+if (function_exists('contentSelector')) {
+    contentSelector($section);
+} else {
+    echo 'La función contentSelector no está definida.';
+}
+?>
+<script src="../resources/js/jquery-3.3.1.min.js"></script>
 <script src="../resources/js/NavToggle.js"></script>
