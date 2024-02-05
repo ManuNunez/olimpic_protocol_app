b79a8a74f4cb7fa7f369e73452cbc010b35e6247
MNunez <manu.nunez.2004@gmail.com>
2024-02-04 19:46:59 -0600

    refactor : NavTtoggle.js refactor

--- src/view/resources/js/NavToggle.js
+++ src/view/resources/js/NavToggle.js
@@ -1,3 +1,11 @@
-document.getElementById('menu-toggle').addEventListener('click', function () {
-    document.getElementById('mobile-menu').classList.toggle('hidden');
+$(document).ready(function() {
+    $('#menu-toggle').on('click', function() {
+        $('#mobile-menu').toggle();
+    });
+
+    $('#mobile-menu a').on('click', function() {
+        var section = $(this).data('section');
+        contentSelector(section);
+        $('#mobile-menu').hide();
+    });
 });
