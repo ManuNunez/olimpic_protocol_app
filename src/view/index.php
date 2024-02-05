263fbd4d762da2f29e0638c6109ab6d5471e86b6
MNunez <manu.nunez.2004@gmail.com>
2024-02-04 23:25:28 -0600

    feat  : all css files from styles imported

--- src/view/index.php
+++ src/view/index.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 /*
 if (!isset($_SESSION['usuario'])) {
@@ -14,16 +13,30 @@ if (!isset($_SESSION['usuario'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Math Contest Protocol</title>
     <link href="resources/main.css" rel="stylesheet">
+    <?php
+    $carpeta_css = 'resources/styles/';
+    $archivos_css = glob($carpeta_css . '*.css');
+
+    foreach ($archivos_css as $archivo) {
+        echo '<link rel="stylesheet" href="' . $archivo . '">' . PHP_EOL;
+    }
+    ?>
 </head>
 
 <body>
-    <?php
-    include('components/nav.php');
-    ?>
+    <?php include('components/nav.php'); ?>
+    
+    <div class="center-container">
+        <div class="container">
 
-    <?php
-    include('/components/footer.php');
-    ?>
+            <?php
+            include_once 'selector.php';
+            contentSelector(isset($_GET['section']) ? $_GET['section'] : '');
+            ?>
+        </div>
+    </div>
+
+    <?php include('components/footer.php'); ?>
 
     <script src="resources/js/jquery-3.3.1.min.js"></script>
     <script src="resources/js/NavToggle.js"></script>
