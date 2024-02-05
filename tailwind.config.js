403557998e7befff924d5505c7340421c51feebf
MNunez <manu.nunez.2004@gmail.com>
2024-02-04 22:12:42 -0600

    tailwind rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/.{html, js, php}"],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{html, php, php}"],
+  extend: {},
   plugins: [],
 }
-
