e787b2e2e0c2f61b66ca5af731f5f19f3f0a0aec
MNunez <manu.nunez.2004@gmail.com>
2024-02-05 00:37:17 -0600

    refactor : script in location is now a general script for every modal

---
+++ src/view/resources/js/modal.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById('modal').classList.remove('hidden');
+}
+
+function closeModal() {
+    document.getElementById('modal').classList.add('hidden');
+}
