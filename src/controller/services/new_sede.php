e05e6071bc46b8942c8ad8cba3aecabca2d8b134
Alexis <alexis.ortiz6646@alumnos.udg.mx>
2024-02-14 23:16:43 -0600

    Update new_sede.php
    reduce the code a lot with a function

--- src/controller/services/new_sede.php
+++ src/controller/services/new_sede.php
@@ -17,69 +17,52 @@ $query = "INSERT INTO Sedes(
 $res = $conn->query($query);
 if($res){ // create a column in the table Sedes
     $query = "SELECT id FROM Sedes
-       WHERE sede_name = '$sede_name' AND status = 1";
-    $sedeIdAnswer = $conn->query($query);
-    if($sedeIdAnswer){ // Get the id from the sede just created
-        $row = $sedeIdAnswer->fetch_assoc();
-        $sede_id = $row['id'];
-        $user_type_id = 3;
-        for($i = 1 ; $i != 3 ; $i++){ // insert 2 new users type Staff
-            $new_staff_account = 'STAFF' . $sede_name . '-' . $i;
-            $new_staff_pass =  md5('STAFF-' .$sede_name  .  '-' . $i . '$');
-            $query = "INSERT INTO USERS(
-            username,
-            password,
-            user_type_id,
-            sede_id
-            ) VALUES (
-            '$new_staff_account',
-            '$new_staff_pass',
-            '$user_type_id',
-            '$sede_id'
-            )";
-            $new_staff_answer = $conn->query($query);
-            if($new_staff_answer){
-                $completeFlag = true;
-            }else{
-                $completeFlag = false;
-            }
-        }
-        $user_type_id = 4;
-        for($i = 1 ; $i != $classroom+1; $i++){ //insert every new user for the classroom acount type 4
-            $new_classroom_account = $sede_name . '-' . $i;
-            $newPass = md5($sede_name . '-' . $i . '$');
-            $query = "INSERT INTO USERS(
-            username,
-            password,
-            user_type_id,
-            sede_id
-            ) VALUES (
-            '$new_classroom_account',
-            '$newPass',
-            '$user_type_id',
-            '$sede_id'
-            )";
-            $new_classroom_answer = $conn->query($query);
-            if($new_classroom_answer){
-                $query_salon_id = "INSERT INTO Salon(
-                sede_id,
-                name
-                )VALUES (
-                '$sede_id',
-                'salon-$i'
-                )";
-                $salon_answer = $conn->query($query_salon_id);
-                if($salon_answer){
-                    $completeFlag = true;
-                }else{
-                    $completeFlag = false;
-                }
-            }else{
-                $completeFlag = false;
-            }
-        }
-    }else{
-        $completeFlag = false;
+    WHERE sede_name = '$sede_name' AND status = 1";
+    $sedeIdAnswer = $conn->query($query);// Get the id from the sede just created
+    $row = $sedeIdAnswer->fetch_assoc();
+    $sede_id = $row['id'];
+    $user_type_id = 3;
+    for($i = 1 ; $i != 3 ; $i++){ // insert 2 new users type Staff
+        $new_staff_account = 'STAFF' . $sede_name . '-' . $i;
+        $new_staff_pass =  md5('STAFF-' .$sede_name  .  '-' . $i . '$');
+        $query = "INSERT INTO USERS(
+        username,
+        password,
+        user_type_id,
+        sede_id
+        ) VALUES (
+        '$new_staff_account',
+        '$new_staff_pass',
+        '$user_type_id',
+        '$sede_id'
+        )";
+        $new_staff_answer = $conn->query($query);
+    }
+    $user_type_id = 4;
+    for($i = 1 ; $i != $classroom+1; $i++){ //insert every new user for the classroom acount type 4
+        $new_classroom_account = $sede_name . '-' . $i;
+        $newPass = md5($sede_name . '-' . $i . '$');
+        $query = "INSERT INTO USERS(
+        username,
+        password,
+        user_type_id,
+        sede_id
+        ) VALUES (
+        '$new_classroom_account',
+        '$newPass',
+        '$user_type_id',
+        '$sede_id'
+        )";
+        $new_classroom_answer = $conn->query($query);
+
+        $query_salon_id = "INSERT INTO Salon(
+        sede_id,
+        name
+        )VALUES (
+        '$sede_id',
+        'salon-$sede_name-$i'
+        )";
+        $salon_answer = $conn->query($query_salon_id);
     }
 }else{
     $completeFlag = false;
