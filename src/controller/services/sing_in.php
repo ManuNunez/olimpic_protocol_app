9415792b433d0c90a2ac60fc2486c613d1cba195
ikercfamoso <iker.covarrubias5286@alumnos.udg.mx>
2024-03-29 01:44:14 -0600

    feat: loggin and sessions

--- src/controller/services/sing_in.php
+++ src/controller/services/sing_in.php
@@ -1,29 +1,54 @@
 <?php
 require '../config/con.php';
 $conn = connection();
-$username = $_REQUEST['username']; // for the username is just a guess
-$pass = md5($_REQUEST['pass']);
+$username = $_POST['username']; // for the username is just a guess
+$pass = md5($_POST['password']);
 
 
 
 $query = "SELECT * FROM USERS
-          WHERE username = '$username' AND
-          password = '$pass' AND 
-          status = 1 AND deleted = 0" ;
-
+            WHERE username = '$username' AND
+            password = '$pass' AND 
+            status = 1 AND deleted = 0" ;
 $answer = $conn->query($query);
 if($answer){
     if($answer->num_rows == 1){
-        // session_start();             # unmark this when session are working.
-        // $_SESSION["logged"] = true;
+        $row = $answer->fetch_assoc();
+        // Asignar el userType y username a la sesión
+        $user_type_id = $row['user_type_id'];
+        $userType = '';
+        switch ($user_type_id) {
+            case 1:
+                $userType = 'manager';
+                break;
+            case 2:
+                $userType = 'organizer';
+                break;
+            case 3:
+                $userType = 'staff';
+                break;
+            case 4:
+                $userType = 'classroom';
+                break;
+            default:
+                $userType = 'unknown';
+        }
+
+        session_start();
+        $_SESSION['logged'] = true;
+        $_SESSION['user_type'] = $userType;
+        $_SESSION['username'] = $row['username'];
         $conn->close();
-        echo 1;
-    }
-    else{
-        echo 0;
+        $ans = json_encode(array(1));
+        echo $ans;
+    }else{
+        $conn->close();
+        $ans = json_encode(array(0, 'User not found'));
+        echo $ans;
     }
 }else{
-    echo 0 .'Error during the query: ' . $conn->error;
+    $conn->close();
+    $ans = json_encode(array(0, $conn->error));
 }
 
 
--- src/view/components/login_form.php
+++ src/view/components/login_form.php
@@ -1,22 +1,26 @@
 <head>
     <link rel="stylesheet" href="resources/main.css">
 </head>
-<div class="bg-gray-800 p-8 rounded shadow-md w-96">
+
+
+<div class="bg-gray-50 p-8 rounded shadow-md w-96">
     <div class="text-center mb-6">
         <h2 class="text-2xl font-semibold">Iniciar Sesión</h2>
     </div>
 
-    <form>
+    <form id="record">
         <div class="mb-4">
-            <label for="username" class="block text-sm font-medium text-gray-300">Nombre de Usuario</label>
-            <input type="text" id="username" name="username" class="mt-1 p-2 w-full bg-gray-700 text-white border rounded-md">
+            <label for="username" class="block text-sm font-medium text-black">Nombre de Usuario</label>
+            <input type="text" id="username" name="username" class="mt-1 p-2 w-full bg-gray-700 text-black border rounded-md" required>
         </div>
 
         <div class="mb-4">
-            <label for="password" class="block text-sm font-medium text-gray-300">Contraseña</label>
-            <input type="password" id="password" name="password" class="mt-1 p-2 w-full bg-gray-700 text-white border rounded-md">
+            <label for="password" class="block text-sm font-medium text-black">Contraseña</label>
+            <input  type="password" id="password" name="password" class="mt-1 p-2 w-full bg-gray-700 text-black border rounded-md" required>
         </div>
 
-        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md w-full">Iniciar Sesión</button>
+        <button type="button" onclick="validateData()" class="bg-blue-500 text-white px-4 py-2 rounded-md w-full">Iniciar Sesión</button>
     </form>
 </div>
+
+<script src="resources/js/login.js"></script>
