b7e2a60124691f3cd6bb1b9abf98a4b5d21941e7
MNunez <manu.nunez.2004@gmail.com>
2024-04-11 16:12:25 -0600

    tailwind error solved(temporal fix)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js, php}"],
+  content: ["./src/view/resources/DO_NOT_DELETE/test.html"],
   extend: {},
   plugins: [],
 }
