525af0e12119520661a7962e765f93c11d304b2e
ManuNunez <manu.nunez.2004@gmail.com>
2024-05-28 14:57:28 -0600

    fix(return sedes):return sedes fixed

--- src/controller/services/return_sedes.php
+++ src/controller/services/return_sedes.php
@@ -1,32 +1,29 @@
 <?php
-//require '../controller/config/con.php';
-require '../config/con.php';
+require '../controller/config/con.php';
+//require '../config/con.php';
 $conn = connection();
 
 $query = "SELECT * FROM Sedes WHERE status = 1";
 $res = $conn->query($query);
-$query_answer_arr = [];
-if ($res) {
-    //$query_answer_arr = ['status'=>1];
-    if($res->num_rows > 0 ){
-        while($row = $res->fetch_assoc()) {
-           $query_answer_arr = ['id' => $row['id'], 'locationName' => $row['sede_name']];
-        }
+if ($res && $res->num_rows > 0 ) {
+
+    $query_answer_arr = [];
+
+    while($row = $res->fetch_assoc()){
+        $sede = array(
+            'id' => $row['id'],
+            'locationName' => $row['sede_name']
+        );
+        $query_answer_arr[] = $sede;
     }
+
     $conn->close();
-} else {
-    echo json_encode(array("status" => 0, "error" => "Error during the query: " . $conn->error));
+    $ans = json_encode($query_answer_arr);
+
+}else{
     $conn->close();
+    $ans = json_encode(array("status" => 0, "error" => "Error during the query: " . $conn->error));
 }
-function getData(){
-    return $query_answer_arr;
-}
-$i = 0;
-foreach ($query_answer_arr as $value){
-    echo $value[$i];
-    $i++;
-}
-echo '<br>'.json_encode($query_answer_arr);
 
 
 ?>
