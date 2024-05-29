2784bca6c7040a493dfc0fee58e9645ac5bdb6a5
Alexis <alexis.ortiz6646@alumnos.udg.mx>
2024-05-28 19:42:20 -0600

    ux Chat

--- src/view/components/chat.php
+++ src/view/components/chat.php
@@ -2,23 +2,41 @@
     <link rel="stylesheet" href="resources/main.css">
 </head>
 
-
-    <div class="w-full px-5 flex flex-col justify-center items-center">
-        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Chat oficial</h1>
-        <section id="chat" class="grid place-items-center mt-5 w-4/5">
-            <div class="flex flex-col w-4/5 leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700 mb-2">
-                <div class="flex items-center space-x-2 rtl:space-x-reverse">
-                    <span class="text-sm font-semibold text-gray-900 dark:text-white">Lorem Ipsum</span>
-                    <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:11</span>
-                </div>
-                <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor</p>
-                <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
+<div class="w-full place-content-center row-end-1 my-10 inline-block" style="height: 600px">
+        <div>
+            <h1 class="text-center mb-1 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Chat oficial</h1>
+        </div>
+    <div class=" w-5/6 place-content-center inline-block rounded-lg shadow-lg content-center">
+        <div class="w-full inline-block  " >
+            <div class="w-5/6 px-5 flex flex-col   ">
+                <section id="chat" class="grid place-items-center  mt-5  w-4/5">
+                    <div class="flex flex-col w-4/5 leading-1.5 p-4 shadow-lg bg-blue-200 rounded-lg mb-2">
+                        <div class="flex items-center space-x-2 rtl:space-x-reverse">
+                            <span class="text-sm font-semibold text-gray-900 dark:text-white">Lorem Ipsum</span>
+                            <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:11</span>
+                        </div>
+                        <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor</p>
+                        <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
+                    </div>
+                </section>
             </div>
-        </section>
-        
+        </div>
+
+        <div class="flex p-4   ">
+            <div class="w-5/6">
+                <input type="text" id="messageInput" class="border-gray-100 border-2 w-full rounded-lg p-3" placeholder="Escribe tu mensaje">
+            </div>
+            <div class="w-1/6 place-content-center flex" >
+                <div>
+                    <button class="bg-blue-300 p-2 rounded-full"  id="sendButton">
+                        <svg width="34px" height="34px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.4" d="M15.712 7.72681L9.89099 13.5478" stroke="#130F26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9.8912 13.548L3.0762 9.381C2.1832 8.835 2.3642 7.488 3.3702 7.197L19.4602 2.549C20.3752 2.284 21.2212 3.138 20.9472 4.05L16.1732 20.014C15.8742 21.014 14.5332 21.186 13.9912 20.294L9.8912 13.548" stroke="#000000" stroke-width="0.8" ></path> </g></svg>
+                    </button>
+                </div>
+
+            </div>
+        </div>
     </div>
-    <input type="text" id="messageInput" placeholder="Escribe tu mensaje">
-    <button id="sendButton">Enviar</button> 
+</div>
     
 
     <script type = "module">
